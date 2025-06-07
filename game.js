005c2e57c8282d3a8c1a77dcfd214b954951dc8c
eastha10 <kdh9158kdh@gmail.com>
2025-06-08 01:22:08 +0900

    localstorage사용 초기화되는 문제 해결

--- game.js
+++ game.js
@@ -8,26 +8,46 @@ const bgmTracks = [
   "fearless-final-boss-battle",
   "epic-battle",
 ];
-//직업 저장 jobs 0: bow, 1: knight, 2: magic witch
+
+// 기본값
 let job = 0;
-//스킨 저장은 위에 배열 bow, knight, magic_witch 참고
 let skin = 0;
-//배경음악 저장 0: stealth-battle, 1: fearless-final-boss-battle, 2: epic-battle
 let music = 0;
-//성 스킨 저장
 let castle = 0;
 let story = 0;
 let storyInterval;
 
+// 새로고침 시 localStorage 초기화
+if (performance.navigation.type === 1) {
+  // 새로고침 감지
+  localStorage.clear();
+}
+
 $(document).ready(function () {
-  // options 버튼 클릭시
-  $(".menu-btn")
-    .eq(1)
-    .on("click", function () {
-      $(".menu").hide();
-      $(".start-logo").hide();
-      $("#setting-wrapper").removeClass("hidden");
-    });
+  // localStorage에서 상태 복원
+  if (localStorage.getItem("job")) {
+    job = parseInt(localStorage.getItem("job"));
+    skin = parseInt(localStorage.getItem("skin"));
+    music = parseInt(localStorage.getItem("music"));
+    castle = parseInt(localStorage.getItem("castle"));
+
+    $(".job").text(jobs[job]);
+    SkinChange();
+    MusicChange();
+    CastleChange();
+  } else {
+    $(".job").text(jobs[job]);
+    JobChange();
+    MusicChange();
+    CastleChange();
+  }
+
+  $(".menu-btn").eq(1).on("click", function () {
+    $(".menu").hide();
+    $(".start-logo").hide();
+    $("#setting-wrapper").removeClass("hidden");
+  });
+
   $(".setting-btn").on("click", function () {
     $(".menu").show();
     $(".start-logo").show();
@@ -40,42 +60,54 @@ $(document).ready(function () {
     $(".job").text(jobs[job]);
     JobChange();
   });
+
   $(".job-next").on("click", function () {
     job = job + 1 + jobs.length;
     job = job % jobs.length;
     $(".job").text(jobs[job]);
     JobChange();
   });
+
   $(".job-skin-before").on("click", function () {
     skin = skin - 1 + 3;
     skin = skin % 3;
     SkinChange();
   });
+
   $(".job-skin-next").on("click", function () {
     skin = skin + 1 + 3;
     skin = skin % 3;
     SkinChange();
   });
+
   $(".bgm-before").on("click", function () {
     music = (music - 1 + bgmTracks.length) % bgmTracks.length;
     MusicChange();
   });
+
   $(".bgm-next").on("click", function () {
     music = (music + 1) % bgmTracks.length;
     MusicChange();
   });
+
   $(".castle-before").on("click", function () {
     castle = (castle - 1 + castleicon.length) % castleicon.length;
     CastleChange();
   });
+
   $(".castle-next").on("click", function () {
     castle = (castle + 1) % castleicon.length;
     CastleChange();
   });
 
-  // gamestart 버튼 클릭 시
+  // 게임 시작: 상태 저장 후 이동
   $(".menu-btn").eq(0).on("click", function () {
-  window.location.href = "story.html";
+    localStorage.setItem("job", job);
+    localStorage.setItem("skin", skin);
+    localStorage.setItem("music", music);
+    localStorage.setItem("castle", castle);
+
+    window.location.href = "story.html";
   });
 });
 
@@ -101,11 +133,13 @@ function SkinChange() {
     $(".job-icon").attr("src", `${magic_witch[skin]}.png`);
   }
 }
+
 function MusicChange() {
   const src = `${bgmTracks[music]}.mp3`;
   $("#bgm").attr("src", src)[0].play();
   $(".bgm-name").text(bgmTracks[music].replace(/-/g, " "));
 }
+
 function CastleChange() {
   $(".castle-icon").attr("src", `${castleicon[castle]}.png`);
 }
